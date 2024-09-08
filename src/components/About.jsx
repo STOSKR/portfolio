6d05601f7bfe3d5c3dbe078fca38b31f77f3a25d
Shiyi Cheng <SCHENG1@upv.edu.es>
2024-09-08 19:48:50 +0200

    aa

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc'
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -31,17 +32,17 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}> Introduction </p>
+        <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] text-justify'
+        className='mt-4 text-secondary text-[17px] max-w-6xl leading-[30px] text-justify'
       >
         I am a 4th-year computer engineering student with a passion for technology, programming, and developing innovative solutions. Throughout my academic journey, I have developed a strong foundation in software development, data structures and algorithms. I have also gained experience in web development, mobile app development, and machine learning. I am a quick learner and collaborative.
       </motion.p>
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className='mt-20 flex flex-wrap gap-12'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
@@ -50,4 +51,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
