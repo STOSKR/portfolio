51fd9af4939f948f3d123abf475612ef1d6e2b1e
Shiyi Cheng <SCHENG1@upv.edu.es>
2024-09-10 23:47:01 +0200

    General changes, contact form checkpoint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { MotionConfig, motion } from "framer-motion";
 
 import { styles } from '../styles'
 import { navLinks } from '../constants'
-import { logo, menu, close } from '../assets'
+import { logo } from '../assets'
 
 const Navbar = () => {
-
   const [active, setActive] = useState("")
   const [toggle, setToggle] = useState(false)
 
   return (
-    <nav className={`${styles.paddingX} w-full flex
-      items-center py-5 fixed top-0 z-20 bg-primary`}>
-
-      <div className='w-full flex justify-between 
-      items-center max-w-7xl mx-auto'>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
           to="./"
           className='flex items-center gap-2'
@@ -28,14 +25,10 @@ const Navbar = () => {
           <p className='text-white text-[18px] font-bold cursor-pointer flex'> Shiyi &nbsp; <span className='sm:block'>Cheng</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
-          {/* En el array navLinks está guardado las diferentes secciones y la función map lo que hace es llamar a cada uno de los elementos del array */}
           {navLinks.map((link) => (
             <li
               key={link.id}
-              className={`${active === link.title
-                ? "text-white"
-                : "text-secondary"
-                } hover:text-white text-[18px] font-medium  cursor-pointer`}
+              className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(link.title)}
             >
               <a href={`#${link.id}`}>{link.title}</a>
@@ -43,25 +36,44 @@ const Navbar = () => {
           ))}
         </ul>
         <div className='sm:hidden flex flex-1 justify-end items-center'>
-          <img
-            src={toggle ? close : menu}
-            alt="menu"
-            className='w-[28px] h-[28px] object-contain cursor-pointer'
-            onClick={() => setToggle(!toggle)}
-          />
-
-          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+          <MotionConfig transition={{ duration: 0.2, ease: "easeInOut" }}>
+            <motion.button
+              initial={false}
+              animate={toggle ? "open" : "closed"}
+              onClick={() => setToggle(!toggle)}
+              className="relative h-[28px] w-[28px] cursor-pointer"
+            >
+              <motion.span
+                variants={VARIANTS.top}
+                className="absolute h-[2px] w-[25px] bg-white"
+                style={{ y: "-50%", left: "50%", x: "-50%", top: "30%" }}
+              />
+              <motion.span
+                variants={VARIANTS.middle}
+                className="absolute h-[2px] w-[25px] bg-white"
+                style={{ left: "50%", x: "-50%", top: "50%", y: "-50%" }}
+              />
+              <motion.span
+                variants={VARIANTS.bottom}
+                className="absolute h-[2px] w-[17px] bg-white"
+                style={{
+                  x: "-50%",
+                  y: "50%",
+                  bottom: "30%",
+                  left: "calc(50% + 5px)",
+                }}
+              />
+            </motion.button>
+          </MotionConfig>
+          <div className={`${toggle ? 'flex' : 'hidden'} p-6 black-gradient absolute top-16 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
             <ul className='list-none flex justify-end items-start flex-col gap-4'>
               {navLinks.map((link) => (
                 <li
                   key={link.id}
-                  className={`${active === link.title
-                    ? "text-white"
-                    : "text-secondary"
-                    } fonts-poppins fonts-medium cursor-pointer text-[16px]`}
+                  className={`${active === link.title ? "text-white" : "text-secondary"} fonts-poppins fonts-medium cursor-pointer text-[16px]`}
                   onClick={() => {
-                    setToggle(!toggle)
                     setActive(link.title)
+                    setToggle(false) // Cerrar el menú después de seleccionar
                   }}
                 >
                   <a href={`#${link.id}`}>{link.title}</a>
@@ -71,8 +83,41 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-    </nav >
+    </nav>
   )
 }
 
 export default Navbar
+
+const VARIANTS = {
+  top: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      top: ["30%", "50%", "50%"],
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      top: ["50%", "50%", "30%"],
+    },
+  },
+  middle: {
+    open: {
+      rotate: ["0deg", "0deg", "-45deg"],
+    },
+    closed: {
+      rotate: ["-45deg", "0deg", "0deg"],
+    },
+  },
+  bottom: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      bottom: ["30%", "50%", "50%"],
+      left: "50%",
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      bottom: ["50%", "50%", "30%"],
+      left: "calc(50% + 5px)",
+    },
+  },
+};
