0ae4c8e9e75cf32fe42e34ccd7d1f7432b87e5da
Shiyi Cheng <SCHENG1@upv.edu.es>
2024-09-12 00:56:31 +0200

    Contact section done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,8 +17,42 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false)
 
-  const handleChange = (e) => { }
-  const handleSubmit = (e) => { }
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    emailjs.sendForm(
+      'service_v12kpjj',
+      'template_65uz7bh',
+      {
+        from_name: form.name,
+        to_name: 'Shiyi',
+        from_email: form.email,
+        to_email: 'shiyicheng13@gmail.com',
+        message: form.message
+      },
+      'user_HRIQzjzYU-MV_5KvP')
+
+      .then(() => {
+        setLoading(false)
+        alert('Thank you! I will get back to you as soon as possible.')
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        })
+
+      }, (error) => {
+        setLoading(false)
+        console.log(error.text)
+
+        alert('Sorry, something went wrong.')
+      })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
